10f6340cc0d669dbabe3676d06cb476e68b079cd
yanghao02 <yang>
2020-08-04 19:36:44 +0800

    condition judge

--- base/condition_judge.go
+++ base/condition_judge.go
@@ -2,22 +2,38 @@ package main
 
 import "fmt"
 
-func test1() {
+func test() {
 	source := 20
 	if source % 2 == 0 {
 		fmt.Printf("%v is 偶数 \n", source)
 	} else {
-		fmt.Printf("%v is 奇数", source)
+		fmt.Printf("%v is 奇数 \n", source)
 	}
 
 	// 在条件语句内声明变量
 	if a := 21; a % 2 == 1 {
-		fmt.Printf("%v is male", a)
+		fmt.Printf("%v is male \n", a)
 	} else {
-		fmt.Printf("%v is female", a)
+		fmt.Printf("%v is female \n", a)
 	}
 }
 
+func testSwitch() {
+	source := 20
+	grade := ""
+	// 默认为 true，通过条件语句进行判断
+	switch {
+	case source > 10:
+		grade = "A"
+	case source > 20:
+		grade = "B"
+	}
+	fmt.Printf("grade is %v", grade)
+
+	//
+}
+
 func main() {
-	test1()
+	test()
+	testSwitch()
 }
