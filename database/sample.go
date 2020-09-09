9e103f344283d36462f5431d72128886afdac8d9
Y-lonelY <luckyyoung666@gmail.com>
2020-09-09 22:40:21 +0800

    feat gorm add create && find methods

--- database/sample.go
+++ database/sample.go
@@ -13,6 +13,33 @@ type User struct {
 	Age  int
 }
 
+type UserFace interface {
+	create()
+	delete()
+	find()
+}
+
+func create(u User) {
+	fmt.Println("create a record ------", u)
+	db.Create(&u)
+}
+
+func delete(u User) {
+	fmt.Println("delete a record ------", u)
+}
+
+func find(u []User) {
+	fmt.Println("find all record ------")
+	// find first record
+	//db.First(&u)
+	// fins all rows
+	db.Find(&u)
+	fmt.Println(u)
+}
+
+// global define db
+var db *gorm.DB
+
 func main() {
 	// step1. connect to the mysql
 	dsn := "root:theless14more@tcp(127.0.0.1:3306)/gro-up?charset=utf8mb4&parseTime=True&loc=Local"
@@ -20,7 +47,7 @@ func main() {
 	if err != nil {
 		panic("failed to connect database!")
 	}
-	db, err := gorm.Open(mysql.New(mysql.Config{
+	db, err = gorm.Open(mysql.New(mysql.Config{
 		Conn: sqlDB,
 	}), &gorm.Config{})
 	if err == nil {
@@ -29,4 +56,11 @@ func main() {
 	// step2. define a struct to reflect the table structure
 	// step3. generate a table
 	_ = db.AutoMigrate(&User{})
+	fmt.Println("---------- connect success -----------")
+
+	// test for create row
+	//create(User{Name: "test", Age: 27})
+
+	// test find rows
+	find([]User{})
 }
