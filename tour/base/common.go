da7adaeafb04a016ce3a0462b43c7ed1348deaa0
Y-lonelY <luckyyoung666@gmail.com>
2023-01-06 11:25:57 +0800

    feat: update base usage

--- tour/base/common.go
+++ tour/base/common.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
+
+func IsNumString(s string) bool {
+	_, err := strconv.ParseFloat(s, 64)
+	return err == nil
+}
 
 func main() {
 	// 字符串转小写
 	str := strings.ToLower("HELLO")
 	fmt.Println(str)
+
+	is_num := IsNumString("123a")
+	fmt.Println(is_num)
 }
--- tour/base/structs.go
+++ tour/base/structs.go
@@ -9,8 +9,15 @@ type Heros struct {
 	B string
 }
 
+// 结构体继承
+type SuperMan struct {
+	Heros
+	Name string
+}
+
 // 通过 `struct{}` 的形式来初始化，通过 `.` 来进行取值（或者重新赋值）
 // 在 go 内通过 `&` 对 structs 进行取址后，不再需要用 `*` 来进行取值操作，go 会默认处理
+// 在 Go 内，对于 struct, map 都应使用取址符，避免 cant assigned error
 func main() {
 	h := Heros{"PA", "UG"}
 	h.A = "SA"
